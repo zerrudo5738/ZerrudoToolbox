eadd3094d0f8e3d918848967fb816e10dab30766
zerrudo5738 <zerrudo5738@students.d211.org>
2019-09-12 13:48:24 -0500

    more apps

---
+++ app/src/main/java/com/example/zerrudotoolbox/fidgetSpinnerMaker.java
@@ -0,0 +1,14 @@
+package com.example.zerrudotoolbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fidgetSpinnerMaker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fidget_spinner_maker);
+    }
+}
