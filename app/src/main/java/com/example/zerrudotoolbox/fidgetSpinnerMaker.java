7be756838780d255b6a0deacde10a0ed51f24ebf
zerrudo5738 <zerrudo5738@students.d211.org>
2019-09-16 13:48:43 -0500

    fidget spinner designs

--- app/src/main/java/com/example/zerrudotoolbox/fidgetSpinnerMaker.java
+++ app/src/main/java/com/example/zerrudotoolbox/fidgetSpinnerMaker.java
@@ -3,6 +3,13 @@ package com.example.zerrudotoolbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class fidgetSpinnerMaker extends AppCompatActivity {
 
@@ -10,5 +17,10 @@ public class fidgetSpinnerMaker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fidget_spinner_maker);
+
+
     }
+
+
 }
+
